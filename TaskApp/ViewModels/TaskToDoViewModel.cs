f9ad90bd98e545c8eb46fa28ec6b6dcb76642f63
Łukasz Charewicz <lukasz.charewicz@microsoft.wsei.edu.pl>
2022-09-24 19:40:16 +0200

    Added TaskToDoViewModel

---
+++ TaskApp/ViewModels/TaskToDoViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskApp.Enums;
+using TaskApp.Models;
+
+namespace TaskApp.ViewModels
+{
+	internal class TaskToDoViewModel : ViewModelBase
+	{
+		private readonly TaskToDo _task;
+
+
+		public string Description => _task.Description;
+		public string CreationTime => _task.CreationTime.ToString("g");
+		public string Deadline => _task.Deadline.ToString("g");
+		public TaskState State => _task.State;
+		public bool IsImportant => _task.IsImportant;
+
+		public TaskToDoViewModel(TaskToDo task)
+		{
+			_task = task;
+		}
+
+	}
+}
