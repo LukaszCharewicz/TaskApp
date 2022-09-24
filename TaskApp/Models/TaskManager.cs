55792be2da28fb27c14714442214b942262eb857
Łukasz Charewicz <lukasz.charewicz@microsoft.wsei.edu.pl>
2022-09-24 19:29:07 +0200

    Added TaskManager class to model

---
+++ TaskApp/Models/TaskManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp.Models
+{
+	internal class TaskManager
+	{
+		private readonly List<TaskToDo> _taskList;
+		public TaskManager()
+		{
+			_taskList = new List<TaskToDo>();
+		}
+
+		public void CreateTask(TaskToDo taskToDo)
+		{
+			_taskList.Add(taskToDo);
+		}
+	}
+}
