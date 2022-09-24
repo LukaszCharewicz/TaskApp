59d39f81faef659f7c7f41f0be66d0a3dc67a1ef
Łukasz Charewicz <lukasz.charewicz@microsoft.wsei.edu.pl>
2022-09-24 19:37:35 +0200

    Added properties to CreateTaskViewModel

---
+++ TaskApp/ViewModels/CreateTaskViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TaskApp.Models;
+using TaskApp.Enums;
+
+namespace TaskApp.ViewModels
+{
+	internal class CreateTaskViewModel : ViewModelBase
+	{
+		private string description;
+
+		public string Description
+		{
+			get
+			{
+				return description;
+			}
+			set
+			{
+				description = value;
+				OnPropertyChanged(nameof(Description));
+			}
+		}
+
+		private DateTime creationTime = DateTime.Now;
+		public DateTime CreationTime
+		{
+			get
+			{
+				return creationTime;
+			}
+		}
+
+		private DateTime deadline = DateTime.Now;
+		public DateTime Deadline
+		{
+			get
+			{
+				return deadline;
+			}
+			set
+			{
+				deadline = value;
+				OnPropertyChanged(nameof(Deadline));
+			}
+		}
+
+		private Enums.TaskState taskState = TaskState.InProgress;
+		public Enums.TaskState TaskState
+		{
+			get
+			{
+				return taskState;
+			}
+			set
+			{
+				taskState = value;
+				OnPropertyChanged(nameof(TaskState));
+			}
+		}
+
+		private bool isImportant = false;
+		public bool IsImportant
+		{
+			get
+			{
+				return isImportant;
+			}
+			set
+			{
+				isImportant = value;
+				OnPropertyChanged(nameof(IsImportant));
+			}
+		}
+
+	}
+}
