5c67fd78dd56b73392c756c29365cf9ffff98304
Łukasz Charewicz <lukasz.charewicz@microsoft.wsei.edu.pl>
2022-09-24 19:27:35 +0200

    Added TaskList class to models folder

---
+++ TaskApp/Models/TaskList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp.Models
+{
+	internal class TaskList
+	{
+		private readonly List<Task> tasks;
+		public TaskList()
+		{
+			tasks = new List<Task>();
+		}
+	}
+}
