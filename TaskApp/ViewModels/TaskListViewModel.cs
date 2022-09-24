284fcaf2d3265c9006564c2d9945e5d67786ed85
Łukasz Charewicz <lukasz.charewicz@microsoft.wsei.edu.pl>
2022-09-24 19:41:25 +0200

    Added TaskListViewModel

---
+++ TaskApp/ViewModels/TaskListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp.ViewModels
+{
+	internal class TaskListViewModel : ViewModelBase
+	{
+		private readonly ObservableCollection<TaskToDoViewModel> _tasksToDo;
+		public IEnumerable<TaskToDoViewModel> TasksToDo => _tasksToDo;
+
+		public TaskListViewModel()
+		{
+			_tasksToDo = new ObservableCollection<TaskToDoViewModel>();
+		}
+	}
+}
