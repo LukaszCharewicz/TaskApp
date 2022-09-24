341b3226056f08f94a43c223630ea93187fe2774
Łukasz Charewicz <lukasz.charewicz@microsoft.wsei.edu.pl>
2022-09-25 00:29:29 +0200

    Added ReloadTaskCommand

---
+++ TaskApp/Commands/ReloadTaskCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TaskApp.Models;
+using TaskApp.ViewModels;
+
+namespace TaskApp.Commands
+{
+	internal class ReloadTaskCommand : AsyncCommandBase
+	{
+		private readonly TaskManager _taskManager;
+		private readonly TaskListViewModel _taskListViewModel;
+
+		public ReloadTaskCommand()
+		{
+		}
+
+		public ReloadTaskCommand(TaskManager taskManager, TaskListViewModel taskListViewModel)
+		{
+			_taskManager = taskManager;
+			_taskListViewModel = taskListViewModel;
+		}
+
+		public override async Task ExecuteAsync(object parameter)
+		{
+			try
+			{
+				IEnumerable<TaskToDo> tasks = await _taskManager.GetAllTasks();
+				_taskListViewModel.UpdateTasks
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Cannot load tasks.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+	}
+}
--- TaskApp/ViewModels/TaskListViewModel.cs
+++ TaskApp/ViewModels/TaskListViewModel.cs
@@ -15,26 +15,32 @@ namespace TaskApp.ViewModels
 	internal class TaskListViewModel : ViewModelBase
 	{
 		private readonly ObservableCollection<TaskToDoViewModel> _tasksToDo;
-		private readonly TaskManager _taskManager;
 
 		public IEnumerable<TaskToDoViewModel> TasksToDo => _tasksToDo;
 		public ICommand NavigateToCreateTaskView { get; }
 
-		public TaskListViewModel(TaskManager taskManager,NavigationService createTaskNavigationService)
+		public ICommand ReloadTaskCommand { get; }
+
+		public TaskListViewModel(TaskManager taskManager, NavigationService createTaskNavigationService)
 		{
 			_taskManager = taskManager;
 			_tasksToDo = new ObservableCollection<TaskToDoViewModel>();
-
+			ReloadTaskCommand = new ReloadTaskCommand(taskManager, this);
 			NavigateToCreateTaskView = new NavigationCommand(createTaskNavigationService);
-			UpdateTasks();
-
-		
 		}
 
-		private void UpdateTasks()
+		public static TaskListViewModel LoadViewModel(TaskManager taskManager, NavigationService createTaskNavigationService)
+		{
+			TaskListViewModel viewModel = new TaskListViewModel(taskManager, createTaskNavigationService);
+			viewModel.ReloadTaskCommand.Execute(null);
+			return viewModel;
+		}
+
+
+		public void UpdateTasks(IEnumerable<TaskToDo> tasks)
 		{ 
 			_tasksToDo.Clear();
-			foreach (var task in _taskManager.GetTasks())
+			foreach (var task in tasks)
 			{
 				TaskToDoViewModel taskToDoViewModel = new TaskToDoViewModel(task);
 				_tasksToDo.Add(taskToDoViewModel);
