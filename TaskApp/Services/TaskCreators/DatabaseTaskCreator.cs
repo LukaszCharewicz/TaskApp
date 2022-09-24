e39b1d728ca80d0cbc5f3d3d9915652523730a67
Łukasz Charewicz <lukasz.charewicz@microsoft.wsei.edu.pl>
2022-09-24 23:43:20 +0200

    Added DatabaseTaskCreator

--- TaskApp/Services/TaskCreators/DatabaseTaskCreator.cs
+++ TaskApp/Services/TaskCreators/DatabaseTaskCreator.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskApp.DBContext;
+using TaskApp.DTOs;
 using TaskApp.Models;
 
 namespace TaskApp.Services.TaskCreators
 {
 	internal class DatabaseTaskCreator : ITaskCreator
 	{
-		public Task CreateTask(TaskToDo task)
+		private readonly TaskDbContextFactory _dbContextFactory;
+
+		public DatabaseTaskCreator(TaskDbContextFactory dbContextFactory)
 		{
-			throw new NotImplementedException();
+			_dbContextFactory = dbContextFactory;
+		}
+		
+		public async Task CreateTask(TaskToDo task)
+		{
+			using (TaskDbContext context = _dbContextFactory.CreateDbContext())
+			{
+				TaskDTO taskDTO = ToTaskDTO(task);
+				context.Tasks.Add(taskDTO);
+				await context.SaveChangesAsync();
+			}
+
+		}
+
+		private TaskDTO ToTaskDTO(TaskToDo task)
+		{
+			return new TaskDTO()
+			{
+				Description = task.Description,
+				CreationTime = task.CreationTime,
+				Deadline = task.Deadline,
+				IsImportant = task.IsImportant
+			};
 		}
 	}
 }
