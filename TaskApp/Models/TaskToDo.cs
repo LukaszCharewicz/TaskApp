5dd685c5cfc28148a1f8ccc948a3a4fb3a3f7a1c
Łukasz Charewicz <lukasz.charewicz@microsoft.wsei.edu.pl>
2022-09-24 19:26:04 +0200

    Added TaskToDo class and TaskState enum

---
+++ TaskApp/Models/TaskToDo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskApp.Enums;
+
+namespace TaskApp.Models
+{
+	internal class TaskToDo
+	{
+		public string Description { get; }
+		public DateTime CreationTime { get; }
+		public DateTime Deadline { get; }
+		public TaskState State { get; }
+		public bool IsImportant { get; }
+
+		public TaskToDo(string description, DateTime creationTime, DateTime deadline, TaskState state, bool isImportant)
+		{
+			Description = description;
+			Deadline = deadline;
+			State = state;
+			IsImportant = isImportant;
+			CreationTime = DateTime.Now;
+		}
+	}
+}
