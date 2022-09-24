50b5faff715f1cc71f432f64cc5ed9fb550c986d
Łukasz Charewicz <lukasz.charewicz@microsoft.wsei.edu.pl>
2022-09-24 23:35:20 +0200

    Added DatabaseTaskProvider and TaskCreator

---
+++ TaskApp/Services/TaskCreators/DatabaseTaskCreator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskApp.Models;
+
+namespace TaskApp.Services.TaskCreators
+{
+	internal class DatabaseTaskCreator : ITaskCreator
+	{
+		public Task CreateTask(TaskToDo task)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- TaskApp/Services/TaskProviders/DatabaseTaskProvider.cs
+++ TaskApp/Services/TaskProviders/DatabaseTaskProvider.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskApp.DBContext;
+using TaskApp.DTOs;
 using TaskApp.Models;
 
 namespace TaskApp.Services.TaskProviders
 {
     internal class DatabaseTaskProvider : ITaskProvider
     {
-        public Task<IEnumerable<TaskToDo>> GetAllTask()
+        private readonly TaskDbContextFactory _dbContextFactory;
+
+        public DatabaseTaskProvider(TaskDbContextFactory dbContextFactory)
         {
-            throw new NotImplementedException();
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task<IEnumerable<TaskToDo>> GetAllTask()
+        {
+            using (TaskDbContext context = _dbContextFactory.CreateDbContext())
+            {
+               IEnumerable<TaskDTO> taskDTOs =  await context.Tasks.ToListAsync();
+               return taskDTOs.Select(x => new TaskToDo(x.Description, x.CreationTime, x.Deadline, x.IsImportant));
+            } 
         }
     }
 }
